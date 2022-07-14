339e59919e8055582112e2e18b409f4cf20d1c71
TseEn Wang <twang224@uic.edu>
2022-07-14 09:30:31 +0800

    remove unnecessary file

--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,12 @@
 const router = require("express").Router();
+const Product = require("../models/Product")
+const {verifyAndAuthorizeToken, verifyAndAuthorizeAdminToken, verifyToken} = require("./verifyToken");
 
-module.exports = router;
+//all users can get product information (without token)
+module.exports = router.get("/product/:id", async(req, res) => {
+    try {
+        const product = Product.findById(req.params.id, );
+    } catch (error) {
+        
+    }
+});
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -26,7 +26,16 @@ const verifyAndAuthorizeToken = (req, res, next) => {
         res.status(403).json("You can only modify your own data !");
        }
     })
-    
 }
 
-module.exports = {verifyToken, verifyAndAuthorizeToken};
+const verifyAndAuthorizeAdminToken = (req, res, next) => {
+    verifyToken(req, res, () => {
+       if(req.user.id === req.params.id && req.user.isAdmin) {
+        next();
+       } else {
+        res.status(403).json("You can only modify your own data !");
+       }
+    })
+}
+
+module.exports = {verifyToken, verifyAndAuthorizeToken, verifyAndAuthorizeAdminToken};
